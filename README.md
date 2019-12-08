7056afbbc4d68947d8259e254966a8a02d42ef39
Rômulo Ferreira <romuloslv@icloud.com>
2019-12-08 14:51:42 -0300

    smtp mail change

---
+++ README.md
@@ -0,0 +1,8 @@
+The guide gets a GitLab instance up and running in minutes. It comes with
+support for:
+
+* Let's Encrypt SSL certificates (via [GitLab's gitlab-ce
+  image](https://hub.docker.com/r/gitlab/gitlab-ce/))
+* Outgoing email (via [Bytemark's SMTP
+  image](https://hub.docker.com/r/bytemark/smtp/))
+* Automatic updates
---
+++ confs/backup-gitlab.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -u
+
+SRC=/etc/gitlab
+DST=/var/opt/gitlab/config-backups
+DATE="`date +%s_%Y_%m_%d`"
+DAYS_TO_KEEP=7
+
+docker-compose exec gitlab gitlab-rake gitlab:backup:create
+
+docker-compose exec gitlab install -m0700 -d "$DST"
+docker-compose exec gitlab tar czf "$DST/$DATE.tar.gz" $SRC
+
+docker-compose exec gitlab find "$DST" -mindepth 1 -maxdepth 1 -type f -name '*.tar.gz' -mtime +$DAYS_TO_KEEP -delete
